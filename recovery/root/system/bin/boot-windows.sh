a02fb52370efb8c29dbe46834c80ea57e82666c4
Arminas Keršys <arminas@ver.lt>
2024-05-28 21:11:26 +0300

    Update boot-windows.sh

--- recovery/root/system/bin/boot-windows.sh
+++ recovery/root/system/bin/boot-windows.sh
@@ -2,14 +2,14 @@
 
 # Flash OOS11 devcfg
 echo "Flashing OOS11 devcfg"
-dd if=/directory/devcfg_oos11.img of=/dev/block/by-name/devcfg_a
-dd if=/directory/devcfg_oos11.img of=/dev/block/by-name/devcfg_b
+dd if=/devcfg_oos11.img of=/dev/block/by-name/devcfg_a
+dd if=/devcfg_oos11.img of=/dev/block/by-name/devcfg_b
 echo "devcfg succesfully flashed"
 
 # Flash UEFI
 echo "Flashing UEFI"
-dd if=/directory/UEFI.img of=/dev/block/by-name/boot_a
-dd if=/directory/UEFI.img of=/dev/block/by-name/boot_b
+dd if=/UEFI.img of=/dev/block/by-name/boot_a
+dd if=/UEFI.img of=/dev/block/by-name/boot_b
 echo "UEFI succesfully flashed"
 
 echo "Rebooting to Windows..."
