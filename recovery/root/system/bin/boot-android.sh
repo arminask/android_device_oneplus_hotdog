39c40d8978ab8b33ba84a7c64d8a73bbd239c651
Arminas Keršys <arminas@ver.lt>
2024-05-29 11:31:55 +0300

    Update boot-android.sh

--- recovery/root/system/bin/boot-android.sh
+++ recovery/root/system/bin/boot-android.sh
@@ -2,14 +2,14 @@
 
 # Flash OOS12 devcfg
 echo "Flashing OOS12 devcfg"
-dd if=/devcfg_oos12.img of=/dev/block/by-name/devcfg_a
-dd if=/devcfg_oos12.img of=/dev/block/by-name/devcfg_b
+dd if=/sdcard/devcfg_oos12.img of=/dev/block/by-name/devcfg_a
+dd if=/sdcard/devcfg_oos12.img of=/dev/block/by-name/devcfg_b
 echo "devcfg succesfully flashed"
 
 # Flash Android boot.img
 echo "Flashing Android boot image"
-dd if=/android-boot.img of=/dev/block/by-name/boot_a
-dd if=/android-boot.img of=/dev/block/by-name/boot_b
+dd if=/sdcard/android-boot.img of=/dev/block/by-name/boot_a
+dd if=/sdcard/android-boot.img of=/dev/block/by-name/boot_b
 echo "Android boot.img succesfully flashed"
 
 echo "Rebooting to Android..."
